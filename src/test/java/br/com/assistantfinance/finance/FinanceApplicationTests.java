e8ee0789d2c52411b58de67621e9433b54f5b7a9
Angélica <angelica_shigematsu@outlook.com>
2024-01-23 11:17:57 -0300

    feat: initial config

---
+++ src/test/java/br/com/assistantfinance/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.assistantfinance.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
